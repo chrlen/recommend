a9533ad17e09fbed40bcc303fddf84b5dc6eaa0e
chrlen <christian.lengert@tuta.io>
2019-08-15 20:47:46 +0200

    comment

--- train_umf.py
+++ train_umf.py
@@ -8,9 +8,9 @@ np.seterr(all='raise')
 train, qualify = rcm.load_data_small()
 nmf = rcm.UnconstrainedMFRecommender()
 nmf.fit(data=train)
-best_model_prediction = nmf.predict(data=qualify).reshape((-1, 1))
-qualifying_prediction = np.append(qualify, np.full((qualify.shape[0], 1), best_model_prediction),axis=1)
-qualifying_file_name  = 'predictions/' + 'final' + nmf.name + str(time.time()) +  ".csv"
-np.savetxt(qualifying_file_name, qualifying_prediction, delimiter=",", newline="\n", encoding="utf-8")
+best_model_prediction = nmf.predict(data=qualify).reshape((-1, 1)) - 1
+#qualifying_prediction = np.append(qualify, np.full((qualify.shape[0], 1), best_model_prediction), axis=1)
+#qualifying_file_name = 'predictions/' + 'final' + nmf.name + str(time.time()) + ".csv"
+#np.savetxt(qualifying_file_name, qualifying_prediction, delimiter=",", newline="\n", encoding="utf-8")
 
 
