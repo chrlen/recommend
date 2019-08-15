574822ca2df7bbc2f5e0900934c08afb94941d0f
chrlen <christian.lengert@tuta.io>
2019-08-15 22:05:22 +0200

    calc cv-error on many lambdas

---
+++ recommend/UnconstrainedRegularisedMFRecommender.py
@@ -0,0 +1,96 @@
+import recommend as rcm
+import numpy as np
+import scipy as scp
+import scipy.sparse as sps
+import scipy.sparse.linalg as spsl
+
+
+class UnconstrainedRegularisedMFRecommender(rcm.Recommender):
+    """  """
+    def __init__(self, **kwargs):
+        rcm.Recommender.__init__(self, "umf", **kwargs)
+        self.converged = False
+        self.factors = kwargs.get('k', 10)
+        self.max_it = kwargs.get('max_it', np.inf)
+        self.alpha = kwargs.get('alpha', 0.001)
+        self.verbose = kwargs.get('verbose', True)
+        self.lambda0 = kwargs.get('lambda', 0.1)
+        self.convergence_error = kwargs.get('convergence_error', 0.001)
+
+    def predict_single(self, user, item):
+        if user >= self.num_users:
+            return self.average
+        if item >= self.num_items:
+            return self.average
+        return self.reconstruction[user,item]
+        #return self.U[user, :].dot(self.V[:, item]).reshape(-1)
+
+    def predict(self, data):
+        res = np.array([ self.predict_single(data[i,0],data[i,1]) for i in range(data.shape[0]) ]).reshape(-1)
+        return res
+
+    def fit(self, data):
+        self.sparse_data = rcm.to_sparse_matrix(data)
+        self.average = np.mean(data[:,2])
+        self.num_users = np.max(data[:, 0]) + 1
+        self.num_items = np.max(data[:, 1]) + 1
+
+        self.mask = self.sparse_data != 0
+        self.mask = self.mask.todense()
+        self.mask = np.logical_not(self.mask)
+
+        self.U = np.random.rand(
+                self.num_users,
+                self.factors
+        )
+
+        self.V = np.random.rand(
+                self.num_items,
+                self.factors
+        )
+
+        self.last_e = 0
+        self.iterations = 0
+        while not self.converged:
+            if self.verbose:
+                print("-------- {} -------".format(self.iterations))
+            R_hat = self.U.dot(self.V.T)
+
+            E = self.sparse_data - R_hat
+            E[self.mask] = 0
+            E = sps.csc_matrix(E)
+
+            self.this_e = spsl.norm(E)
+            if self.verbose:
+                print(np.abs(self.this_e - self.last_e))
+
+            temp_U = self.U
+
+            self.U = self.U * (1-self.alpha * self.lambda0) + self.alpha * E.dot(self.V)
+            self.V = self.V * (1-self.alpha * self.lambda0) + self.alpha * E.T.dot(temp_U)
+
+            if self.iterations >= self.max_it:
+                print("Maximum iterations reached")
+                self.converged = True
+
+            if self.iterations > 0:
+                diff = np.abs(self.last_e - self.this_e)
+                if diff < self.convergence_error:
+                    print("Low Error difference! Finished: ", diff)
+                    self.converged = True
+            self.last_e = self.this_e
+            self.iterations +=1
+        self.reconstruction = self.U.dot(self.V.T)
+
+if __name__ == '__main__':
+    import os
+    train = np.genfromtxt(os.path.join("../", "data", "train.csv"), delimiter=",", dtype=np.int)
+    sgd = rcm.UnconstrainedMFRecommender(
+        **{
+            'max_it': np.inf,
+            'lambda0': 0.1
+        }
+    )
+    err = rcm.cross_validation(sgd, train, 5, err=rcm.rmse)
+    print('Error: ', err)
+
---
+++ train_urmf.py
@@ -0,0 +1,34 @@
+import os
+import numpy as np
+import recommend as rcm
+import time
+
+np.seterr(all='raise')
+
+train = rcm.load_data()
+qualify = rcm.load_qualifying_data()
+
+lambdas = [10 ** i for i in np.arange(0.001, 100)]
+
+nmf = rcm.UnconstrainedRegularisedMFRecommender(
+    **{
+        'max_it': np.inf,
+        'verbose': False
+    }
+)
+pred_err = [
+    rcm.cross_validation(model=rcm.UnconstrainedRegularisedMFRecommender(
+        **{
+            'max_it': np.inf,
+            'verbose': False,
+            'lambda0': l,
+            'convergence_error': 0.001
+        }
+    ), data=train, err=rcm.rmse) for l in lambdas
+]
+
+nmf.fit(data=train)
+best_model_prediction = nmf.predict(data=qualify).reshape((-1, 1)) - 1
+qualifying_prediction = np.append(qualify, np.full((qualify.shape[0], 1), best_model_prediction), axis=1)
+qualifying_file_name = 'predictions/' + 'final' + nmf.name + str(time.time()) + ".csv"
+np.savetxt(qualifying_file_name, qualifying_prediction, delimiter=",", newline="\n", encoding="utf-8")
