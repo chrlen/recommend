6388d755853f8b40e8cd947452d0e167614d1833
chrlen <christian.lengert@tuta.io>
2019-08-15 18:45:42 +0200

    add UMF to eval

--- eval.py
+++ eval.py
@@ -8,11 +8,11 @@ train = rcm.load_data()
 qualifying_data = rcm.load_qualifying_data()
 
 predictors = [
-    rcm.SVDPPRecommender(
-    learning_rate=0.005,
-    regularisation_factor=0.001,
-    maxit=10,
-
+    rcm.UnconstrainedMFRecommender(
+        **{
+            'max_it': np.inf,
+            'alpha': 0.001
+        }
     )
 ]
 
@@ -25,19 +25,19 @@ best = np.argsort(
 best_model = pred_err[0][0]
 best_model_err = str(pred_err[best][1])
 best_model.fit(data=train)
-best_model_prediction = best_model.predict(data=qualifying_data).reshape((-1, 1))
+best_model_prediction = best_model.predict(data=qualifying_data).reshape((-1, 1)) - 1
 
 #conf = rcm.confusion(train[:, 2], best_model_prediction)
 
-qualifying_prediction = np.append(qualifying_data, np.full((qualifying_data.shape[0], 1), best_model_prediction),
-                                  axis=1)
-qualifying_file_name  = 'predictions/' + str(best_model_err) + '_' +  best_model.name + str(time.time()) +  ".csv"
-
-np.savetxt(qualifying_file_name, qualifying_prediction,
-           delimiter=",", newline="\n", encoding="utf-8")
-
-model_file_name = 'models/' + best_model_err + '_' +  best_model.name + ".pcl"
-
-best_model.save(model_file_name=model_file_name)
+# qualifying_prediction = np.append(qualifying_data, np.full((qualifying_data.shape[0], 1), best_model_prediction),
+#                                   axis=1)
+# qualifying_file_name  = 'predictions/' + str(best_model_err) + '_' +  best_model.name + str(time.time()) +  ".csv"
+#
+# np.savetxt(qualifying_file_name, qualifying_prediction,
+#            delimiter=",", newline="\n", encoding="utf-8")
+#
+# model_file_name = 'models/' + best_model_err + '_' +  best_model.name + ".pcl"
+#
+# best_model.save(model_file_name=model_file_name)
 
 
